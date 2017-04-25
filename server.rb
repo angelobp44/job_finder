5f66e591a584ff5bd3c53f2ee20c2101cfb83552
John Doe <johndoe@example.com>
2017-04-24 20:08:50 -0400

    craigslist

--- server.rb
+++ server.rb
@@ -1,4 +1,21 @@
-require 'sinatra'
+require 'Sinatra'
+require "httparty"
+url = "https://www.indeed.com/jobs?q=software+engineer&l=Boca+Raton%2C+FL"
+
+
+
+response = HTTParty.get url
+# puts response.body
+
+
+dom = Sinatra::HTML(response.body)
+
+job_titles =
+  dom.css('.jobtitle').map do |obj|
+    obj.content
+  end
+  p job_titles
+
 
   get '/' do
     "Hello! "
