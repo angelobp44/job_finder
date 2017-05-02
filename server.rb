319b42c809344e887ee97786f94772b4d58f259b
John Doe <johndoe@example.com>
2017-05-02 18:18:37 -0400

    first job finder

--- server.rb
+++ server.rb
@@ -1,25 +1,19 @@
-require 'Sinatra'
+require 'sinatra'
 require "httparty"
+require 'nokogiri'
 url = "https://www.indeed.com/jobs?q=software+engineer&l=Boca+Raton%2C+FL"
-
-
 
 response = HTTParty.get url
 # puts response.body
 
 
-dom = Sinatra::HTML(response.body)
+dom = Nokogiri::HTML(response.body)
 
-job_titles =
+get '/' do
+  job_titles =
+
   dom.css('.jobtitle').map do |obj|
     obj.content
   end
   p job_titles
-
-
-  get '/' do
-    "Hello! "
-  end
-  get '/' do
-    "Welcome to my website!"
-  end
+end 
